90ba1dc3a0648d9e31cba33a6c656740c4b76e45
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-07-24 22:18:55 +0000

    done with task 0 and 1

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,4 @@
-export function getResponseFromAPI() {
-	return new Promise(() => {});
+// Return a new Promise object
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+// Return a promise. The parameter is a boolean.
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success'});
+	}else {
+     reject(Error('The fake API is not working currently'));
+	}
+  });
+}
