6bd15239f7d4ea92be10ef36ac5341aac77352c3
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-07-24 23:09:05 +0000

    done with task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
-//Three handlers to the function
-export function handleResponseFromAPI(promise){
+/**
+ * Attaches handlers to a given promise to handle resolution, rejection, and logging.
+*/
+export default function handleResponseFromAPI(promise) {
   return promise
-	.then(() => ({ status: 200, body: 'success' }))
-	.catch(() => new Error())
-	.finally(() => console.log('Got a response from the API'));
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
