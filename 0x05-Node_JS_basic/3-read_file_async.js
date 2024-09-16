a3ac175e46360a79aa34b8d67787d5aae5bb7684
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-16 22:51:13 +0000

    feat: created an asyncronous version of the countStudent function that reads the database.csv files asyncronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+const fs = require('node:fs').promises;
+
+const studentsInCourse = function * (course, details) {
+  for (let x = 0; x < details.length; x++) {
+    const temp = details[x].split(',');
+    if (temp[temp.length - 1] === course) {
+      yield temp[0];
+    }
+  }
+};
+
+const list = function (students) {
+  const result = [];
+  for (const student of students) {
+    result.push(student);
+  }
+  return result;
+};
+
+const countStudents = async function (path) {
+  try {
+    const data = await fs.readFile(`${path}`, 'utf8');
+
+    const content = data.split('\n');
+    content.shift();
+    content.pop();
+
+    const csStudents = list(studentsInCourse('CS', content));
+    const sweStudents = list(studentsInCourse('SWE', content));
+
+    console.log(`Number of students: ${content.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${[...csStudents].join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${[...sweStudents].join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
